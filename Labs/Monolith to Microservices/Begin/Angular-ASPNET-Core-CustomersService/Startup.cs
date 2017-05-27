cd2b1d2b5582ac13938d8867e5877f931fe8a4c5
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-05-27 14:58:05 -0400

    Initial check-in

---
+++ Labs/Monolith to Microservices/Begin/Angular-ASPNET-Core-CustomersService/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MonolithToMicroservices.Repository;
+using Swashbuckle.Swagger.Model;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Http;
+
+namespace MonolithToMicroservices
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Add PostgreSQL support
+            //services.AddEntityFrameworkNpgsql()
+            //  .AddDbContext<CustomersDbContext>(options =>
+            //     options.UseNpgsql(Configuration["Data:DbContext:CustomersPostgresConnectionString"]));
+
+            //Add SQL Server support
+            //services.AddEntityFrameworkSqlServer()
+            //    .AddDbContext<CustomersDbContext>(options =>
+            //       options.UseSqlServer(Configuration["Data:DbContext:CustomersSqlServerConnectionString"]));
+            
+            //Add SqLite support
+            services.AddEntityFrameworkSqlite()
+                .AddDbContext<CustomersDbContext>(options =>
+                   options.UseSqlite(Configuration["Data:DbContext:CustomersSqliteConnectionString"]));
+
+            services.AddMvc();
+
+            services.AddScoped<ICustomersRepository, CustomersRepository>();
+            services.AddScoped<ILookupRepository, LookupRepository>();
+            services.AddScoped<IOrdersRepository, OrdersRepository>();
+            services.AddTransient<CustomersDbSeeder>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory, 
+            CustomersDbSeeder customersDbSeeder)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            // Serve wwwroot as root
+            app.UseFileServer();
+
+            // Serve /node_modules as a separate root (for packages that use other npm modules client side)
+            // Added for convenience for those who don't want to worry about running 'gulp copy:libs'
+            // Only use in development mode!!
+            app.UseFileServer(new FileServerOptions()
+            {
+                // Set root of file server
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "node_modules")),
+                RequestPath = "/node_modules",
+                EnableDirectoryBrowsing = false
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                //https://github.com/aspnet/JavaScriptServices/blob/dev/samples/angular/MusicStore/Startup.cs
+                routes.MapSpaFallbackRoute("spa-fallback", new { controller = "Home", action = "Index" });
+
+            });
+
+            customersDbSeeder.SeedAsync(app.ApplicationServices).Wait();
+        }
+    }
+}
---
+++ Labs/Monolith to Microservices/Begin/Angular-ASPNET-Core-CustomersService/aspnetcore.development.dockerfile
@@ -0,0 +1,10 @@
+FROM microsoft/dotnet:1.1.1-sdk
+
+MAINTAINER Dan Wahlin
+
+ENV DOTNET_USE_POLLING_FILE_WATCHER=1
+ENV ASPNETCORE_URLS=http://*:5000
+
+WORKDIR /var/www/aspnetcoreapp
+
+CMD ["/bin/bash", "-c", "dotnet restore && dotnet run"]
